4c087b9d28705d43886b885fc857aa0ee269594b
DmitryOgorodnikov <65391963+DmitryOgorodnikov@users.noreply.github.com>
2021-10-20 12:11:43 +0300

    Подключение БД

--- SUO/SUO/settings.py
+++ SUO/SUO/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import posixpath
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,8 +78,10 @@ WSGI_APPLICATION = 'SUO.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'SUOwner',
+        'PASSWORD': '1234',
     }
 }
 
